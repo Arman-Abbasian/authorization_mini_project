f44daf974957e4aa8abb1d144afb93f7f1037176
Arman Abbasian <abasian.arman@gmail.com>
2023-04-28 18:56:18 +0430

    run the web server

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const { ErrorHandler, NotFoundError } = require("./utils/errorHandler");
+const {AllRoutes}=require("./routers/index.routes")
 const app=express();
 require("./config/mongo.config");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+app.use(AllRoutes)
 app.use(NotFoundError);
 app.use(ErrorHandler);
 app.listen(3000,()=>{
---
+++ routers/index.routes.js
@@ -0,0 +1,6 @@
+const { Router } = require("express");
+
+const router=Router();
+module.exports={
+    AllRoutes:router
+}
